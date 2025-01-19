4d8fa0826afad970f9c442a3768a4e35af70a97e
Ddtester5 <dd5892631@gmail.com>
2025-01-19 15:03:21 +0000

    1

--- parse-test/parse_fn/utils/openai/ai_client.ts
+++
@@ -1,6 +0,0 @@
-import OpenAI from "openai";
-
-export const client = new OpenAI({
-  apiKey: "",
-  baseURL: "http://localhost:1337/v1",
-});
--- parse-test/prisma/migrations/20250104102233_add_parsedtitles/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "ParsedTitles" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-
-    CONSTRAINT "ParsedTitles_pkey" PRIMARY KEY ("id")
-);
