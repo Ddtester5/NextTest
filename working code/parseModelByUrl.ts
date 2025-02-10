786ba0b0085307698a08a5123a632d2438985a20
Ddtester5 <dd5892631@gmail.com>
2025-02-10 10:27:25 +0300

    Update parseModelByUrl.ts

--- working code/parseModelByUrl.ts
+++ working code/parseModelByUrl.ts
@@ -1,1 +1,4 @@
-
+import {Page} from "@playwright/test"
+export const parseModelByUrl=async(modelUrl:string,page:Page)/>{
+await page.goto(`https://www.gsmarena.com/${modelUrl},{waitUnyil:"domcontentloaded"})
+}
