5ca1380d68da9ed19e81e7b3c422daa0c827eb38
Ddtester5 <dd5892631@gmail.com>
2025-02-10 10:07:43 +0300

    Update parse_brands_and_models.ts

--- working code/parse_brands_and_models.ts
+++ working code/parse_brands_and_models.ts
@@ -1,42 +1,41 @@
-import {Page} from "@playwright/test";
+import { Page } from "@playwright/test";
 
-export const parseModelsByBrand = async(brandUrl:string,page:Page)=>{
-  const allModelsUrl =[]
-  let currentPage:string|null = `https://www.gsmarena.com/${brandUrl}`;
-  while(currentPage){
-page.goto(currentPage,{waitUnyil:"domcontentloaded"})
-  
-  //add locator
-  const allModelsSinlePage = await page.locator("").evaluateAll(
-    (el)=>({
-      return el.map(
-      (e)=>({
-        modelUrl:e.querySelector("a").getAttribute("href")
-      })
-      ).filter(
-(e)=>e.modelUrl!== null && e.modelUrl !== undefined
-      )
-    })
-  )
-  allModelsUrl= allModelsUrl.concat(allModelsSinlePage)
+export const parseModelssrlByBrand = async (brandUrl: string, page: Page):string[] => {
+  const allModelsUrl: string[] = [];
+  let currentPage: string | null = `https://www.gsmarena.com/${brandUrl}`;
 
-        // Проверка на наличие кнопки "Next page"
-        const nextPageElement = await page.locator(".pages-next").nth(0);
-        const isDisabled = await nextPageElement.evaluate(
-          (el) =>
-            el.classList.contains("disabled") ||
-            el.getAttribute("href") === "#",
-        );
+  while (currentPage) {
+    // Переход на текущую страницу
+    await page.goto(currentPage, { waitUntil: "domcontentloaded" });
 
-        if (isDisabled) {
-          currentPage = null; // Завершаем цикл
-        } else {
-          currentPage = await nextPageElement.getAttribute("href");
-          if (currentUrl) {
-            currentUrl = `https://www.gsmarena.com/${currentUrl}`;
-          }
-        }
+    // Локатор для всех моделей на странице
+    const allModelsSinglePage = await page
+      .locator(".makers .main-review a")
+      .evaluateAll((elements) =>
+        elements
+          .map((e) => e.getAttribute("href"))
+          .filter((href) => href !== null && href !== undefined),
+      );
+
+    // Добавляем URL моделей в общий массив
+    allModelsUrl.push(...allModelsSinglePage);
+
+    // Проверка на наличие кнопки "Next page"
+    const nextPageElement = page.locator(".pages-next").first();
+    const isDisabled = await nextPageElement.evaluate(
+      (el) =>
+        el.classList.contains("disabled") || el.getAttribute("href") === "#",
+    );
+
+    if (isDisabled) {
+      currentPage = null; // Завершаем цикл
+    } else {
+      currentPage = await nextPageElement.getAttribute("href");
+      if (currentPage) {
+        currentPage = `https://www.gsmarena.com/${currentPage}`;
+      }
+    }
   }
-  }
-  return allModelsUrl
-}
+
+  return allModelsUrl.reverse();
+};
