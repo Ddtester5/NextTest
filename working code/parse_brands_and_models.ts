677792ac78f1292a93a922ca819cd6e88bcd40de
Ddtester5 <dd5892631@gmail.com>
2025-02-10 09:58:26 +0300

    Update parse_brands_and_models.ts

--- working code/parse_brands_and_models.ts
+++ working code/parse_brands_and_models.ts
@@ -1,1 +1,42 @@
+import {Page} from "@playwright/test";
 
+export const parseModelsByBrand = async(brandUrl:string,page:Page)=>{
+  const allModelsUrl =[]
+  let currentPage:string|null = `https://www.gsmarena.com/${brandUrl}`;
+  while(currentPage){
+page.goto(currentPage,{waitUnyil:"domcontentloaded"})
+  
+  //add locator
+  const allModelsSinlePage = await page.locator("").evaluateAll(
+    (el)=>({
+      return el.map(
+      (e)=>({
+        modelUrl:e.querySelector("a").getAttribute("href")
+      })
+      ).filter(
+(e)=>e.modelUrl!== null && e.modelUrl !== undefined
+      )
+    })
+  )
+  allModelsUrl= allModelsUrl.concat(allModelsSinlePage)
+
+        // Проверка на наличие кнопки "Next page"
+        const nextPageElement = await page.locator(".pages-next").nth(0);
+        const isDisabled = await nextPageElement.evaluate(
+          (el) =>
+            el.classList.contains("disabled") ||
+            el.getAttribute("href") === "#",
+        );
+
+        if (isDisabled) {
+          currentPage = null; // Завершаем цикл
+        } else {
+          currentPage = await nextPageElement.getAttribute("href");
+          if (currentUrl) {
+            currentUrl = `https://www.gsmarena.com/${currentUrl}`;
+          }
+        }
+  }
+  }
+  return allModelsUrl
+}
